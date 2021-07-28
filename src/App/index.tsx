2018ab9c3acb66b4550cbbaf44f195a7df6d8d2f
bendotman <ben.han@dotman.co.kr>
2021-07-29 00:26:03 +0900

    first commit

---
+++ src/App/index.tsx
@@ -0,0 +1,19 @@
+/**
+ *
+ * App
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import * as React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Let's Create something greate!
+      </header>
+    </div>
+  );
+}
